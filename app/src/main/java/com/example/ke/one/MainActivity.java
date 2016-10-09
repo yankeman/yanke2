3dec166174b1c4f6c7ee2648e58e452c43219232
Your Name <you@example.com>
2016-10-09 18:57:47 +0800

    First

--- app/src/main/java/com/example/ke/one/MainActivity.java
+++ app/src/main/java/com/example/ke/one/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.ke.one;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
